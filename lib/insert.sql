eb5cad79a1e1b441c2b8fe9634cbf003a4070940
Tanya Hodges <tanyac52003@gmail.com>
2020-06-22 18:09:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,2 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, male, brown, )
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'male', 'brown', 'calm', 1 );
+
