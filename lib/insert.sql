4a8efed555c48f6c150e7a38e5d4fb63519c281b
Tanya Hodges <tanyac52003@gmail.com>
2020-06-22 18:12:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'male', 'brown', 'calm', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, 'male', 'black', 'aggressive', 0 );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 3, 'female', 'brown', 'relaxed', 1 );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'male', 'brown', 'calm', 1 );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 8, 'female', 'black', 'calm', 0 );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, 'male', 'black', 'aggressive', 0 );
+
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'male', 'brown', 'calm', 1 );
 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'male', 'brown', 'calm', 1 );
