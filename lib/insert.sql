17f3d70e34cbc2d72e2739829655c12da4489860
Tanya Hodges <tanyac52003@gmail.com>
2020-06-22 18:14:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -10,6 +10,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Meliss
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, 'male', 'black', 'aggressive', 0);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'male', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'female', 'brown', 'relaxed', 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'male', 'brown', 'calm', 1 );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 6, 'male', 'black', 'aggressive', 1);
